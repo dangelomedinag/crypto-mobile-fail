02e6eae48bbe83c4d8f791ee712b7175e4f1cd29
dangelomedinag <dangelomedinag@gmail,com>
2022-11-19 11:34:11 -0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let arr = [];
+	const add = () => (arr = [{ id: crypto.randomUUID() }, ...arr]);
+</script>
+
+<button on:click={add}>add</button>
+{#each arr as item (item.id)}
+	<p>{item.id}</p>
+{/each}
